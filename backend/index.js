3b69975df07104914f01948ba3b45c2c778f8019
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-01 20:50:20 +0300

    profilePage

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,27 @@ const mock = { login: "test", password: "test" };
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const contacts = [{ name: "max", number: "9111", id: "123123123" }];
+
 app.post("/auth", (req, res) => {
-  if (req.body.login === mock.login && req.body.password === mock.password) {
+  const { login, password } = req.body;
+  if (login === mock.login && password === mock.password) {
     res.status(200).json({ auth: "true", res: "OK" });
   } else {
     res.status(403).json({ auth: "false", res: "неверные credentials" });
   }
+});
+
+app.post("/add", (req, res) => {
+  const { name, number, id } = req.body;
+  const contact = { name, number, id: Math.random() * 1000 };
+  console.log(req.body);
+  contacts.push(contact);
+  res.status(200).json({ res: "ok", contact });
+});
+
+app.get("/getContacts", (req, res) => {
+  res.json(contacts);
 });
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const ProfilePage = () => {
+  const [fieldsData, setFieldsData] = useState({ name: "", number: "" });
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch("/getContacts")
+      .then((res) => res.json())
+      .then((data) => setContacts(data));
+  }, []);
+
+  const handleChange = (e) => {
+    setFieldsData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(fieldsData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.res === "ok") {
+          setContacts((prev) => [...prev, data.contact]);
+        }
+      }) //react toast
+      .catch((err) => console.log(err)); //react toast
+  };
+
   return (
     <div>
-      <p>it's profile page</p>
+      <h3>Добавить контакт</h3>
+      <input placeholder="имя" name="name" onChange={(e) => handleChange(e)} />
+      <input
+        placeholder="номер"
+        name="number"
+        onChange={(e) => handleChange(e)}
+      />
+      <button onClick={(e) => handleSubmit(e)}>Войти</button>
+
+      <h3>Список контактов</h3>
+      {contacts.length &&
+        contacts.map((el) => {
+          return (
+            <div key={el.id}>
+              <span>
+                {el.name} - {el.number}
+              </span>
+              <span> X</span>
+            </div>
+          );
+        })}
     </div>
   );
 };
