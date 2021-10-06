bb5ec5973cae5331d7e79e714920be2a8782f2f2
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-06 18:39:20 +0300

    ui

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Paper } from "@material-ui/core";
-import { Grid } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
-import { CardActionArea } from "@material-ui/core";
-import { Card } from "@material-ui/core";
-import { CardContent, TextField } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
+import {
+  Grid,
+  Typography,
+  CardActionArea,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+  Paper,
+} from "@material-ui/core";
 
 export const ProfilePage = () => {
   const [fieldsData, setFieldsData] = useState({ name: "", number: "" });
@@ -50,95 +53,107 @@ export const ProfilePage = () => {
   return (
     <>
       <div className="main__form">
-        <Typography className="form main__title" variant="h6" gutterBottom>
-          Add contact
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="name"
-              name="name"
-              label="Name"
-              fullWidth
-              variant="standard"
-              onChange={(e) => handleChange(e)}
-            />
+        <div className="form__content">
+          <Typography className="form main__title" variant="h6" gutterBottom>
+            Add contact
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="name"
+                name="name"
+                label="Name"
+                fullWidth
+                variant="standard"
+                onChange={(e) => handleChange(e)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="number"
+                name="number"
+                label="Number"
+                fullWidth
+                variant="standard"
+                onChange={(e) => handleChange(e)}
+              />
+            </Grid>
+            <Grid className="button__wrapper" item xs={12} sm={12}>
+              <Button
+                className="button__add"
+                type="submit"
+                variant="contained"
+                onClick={(e) => handleSubmit(e)}
+              >
+                Add
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="number"
-              name="number"
-              label="number"
-              fullWidth
-              variant="standard"
-              onChange={(e) => handleChange(e)}
-            />
-            {/* 
-        <input
-          type="text"
-          placeholder="Search (name)..."
-          value={searchContact}
-          onChange={(event) => handleClick(event)}
-        /> */}
-          </Grid>
-          <Button
-            className="button__add"
-            type="submit"
-            sx={{ mt: 3, ml: 1 }}
-            variant="contained"
-            onClick={(e) => handleSubmit(e)}
-          >
-            Add
-          </Button>
+        </div>
+      </div>
+      <div className="wrapper__input">
+        <Grid item xs={12} sm={12}>
+          <TextField
+            className="input__search"
+            id="search"
+            name="search"
+            label="Search (name)..."
+            value={searchContact}
+            variant="standard"
+            onChange={(event) => handleClick(event)}
+          />
         </Grid>
       </div>
-      <Grid style={{ marginTop: 20 }}>
-        {contacts.length &&
-          contacts
-            .filter((val) =>
-              val.name.toLowerCase().includes(searchContact.toLowerCase())
-            )
-            .map((el) => {
-              return (
-                <Paper elevation={3}>
-                  <Typography color="inherit">
-                    <Grid style={{ marginTop: 20 }}>
-                      <CardActionArea component="a" href="#">
-                        <Card key={el.id}>
-                          <div>
-                            <CardContent>
-                              <Typography
-                                variant="subtitle1"
-                                color="textSecondary"
-                              >
-                                {el.name}
-                              </Typography>
-                              <Typography variant="subtitle1" paragraph>
-                                {el.number}
-                              </Typography>
-                              <Typography variant="subtitle1" color="primary">
-                                <Button
-                                  type="submit"
-                                  sx={{ mt: 3, ml: 1 }}
-                                  variant="contained"
-                                  color="primary"
-                                  onClick={() => handleDelete(el.id)}
+      <div className="main__container">
+        <Grid className="contact__wrapper" Grid item xs={12} sm={6}>
+          {contacts.length &&
+            contacts
+              .filter((val) =>
+                val.name.toLowerCase().includes(searchContact.toLowerCase())
+              )
+              .map((el) => {
+                return (
+                  <Paper elevation={3}>
+                    <Typography color="inherit">
+                      <Grid>
+                        <CardActionArea>
+                          <Card>
+                            <div>
+                              <CardContent className="card__content">
+                                <Typography component="h2" variant="h5">
+                                  Name: {el.name}
+                                </Typography>
+                                <Typography
+                                  variant="subtitle1"
+                                  color="textSecondary"
                                 >
-                                  delete
-                                </Button>
-                              </Typography>
-                            </CardContent>
-                          </div>
-                        </Card>
-                      </CardActionArea>
-                    </Grid>
-                  </Typography>
-                </Paper>
-              );
-            })}
-      </Grid>
+                                  Number: {el.number}
+                                </Typography>
+                                <Typography variant="subtitle1" paragraph>
+                                  <Button
+                                    className="button__delete"
+                                    type="submit"
+                                    sx={{ mt: 3, ml: 1 }}
+                                    variant="contained"
+                                    color=""
+                                    onClick={() => handleDelete(el.id)}
+                                  >
+                                    delete
+                                  </Button>
+                                </Typography>
+                              </CardContent>
+                            </div>
+                          </Card>
+                        </CardActionArea>
+                      </Grid>
+                    </Typography>
+                  </Paper>
+                );
+              })}
+        </Grid>
+      </div>
     </>
   );
 };
